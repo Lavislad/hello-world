bf87fbb5a9096e473e163998a9f8d875a4023cdd
Lavislad <143618582+Lavislad@users.noreply.github.com>
2024-12-11 10:32:57 +0300

    Add files via upload

---
+++ UnitTestsProject/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\\UnitTestsProject\Functions.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int x = 5;
+			int y = 10;
+			Assert::AreEqual(15, func1(x, y));
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			int x = 10;
+			int y = 5;
+			Assert::AreEqual(5, func2(x, y));
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			int x = 10;
+			int y = 5;
+			Assert::AreEqual(50, func3(x, y));
+		}
+	};
+}
---
+++ UnitTestsProject/UnitTestsProject/Functions.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+int func1(int x, int y) {
+	int c;
+	c = x + y;
+	return c;
+}
+
+int func2(int x, int y) {
+	int c;
+	c = x - y;
+	return c;
+}
+
+int func3(int y, int x) {
+	int c;
+	c = x * y;
+	return c;
+}
